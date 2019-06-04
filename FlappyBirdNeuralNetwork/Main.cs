62573102f325248d3cc56b175d418f6161fed1e1
João Paulo de Oliveira <joao.oliveira@neppoti.com.br>
2019-06-04 18:45:32 -0300

    Just planning the classes

---
+++ FlappyBirdNeuralNetwork/Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+// using Neuron;
+namespace FlappyBirdNeuralNetwork{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+    class Neuron{
+        private float limiar;
+        private float output;
+        private List<float>  weightVector = new List<float>();
+        private List<float>  inputLayer = new List<float>();
+        float transferencyFunction(float input){
+            return input;
+        }
+
+        float process(){
+            for
+            return output;
+        }
+
+    }
+}
